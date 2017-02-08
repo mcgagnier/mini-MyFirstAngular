ee72616ba6b59adb117673e0e0d5b84f328aafbe
mcgagnier <mcgagnier@yahoo.com>
2017-02-07 19:20:46 -0700

    done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 angular.module('friendsList', [])
-    .controller(;mainCtrl, ['scope', function($scope){
-        
-    }])
+    .controller('mainCtrl', function($scope){
+        $scope.message = "hola friends";
+        $scope.friends = ["Brandon", "Dyllon", "Tyler"];
+
+        $scope.addFriend = function(name) {
+            $scope.friends.push(name);
+        }
+    })
